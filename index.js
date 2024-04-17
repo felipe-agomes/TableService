0b71fc5cc452504ee6e7e2fcded0443ce1ca27de
Felipe <falmeidagomes13@gmail.com>
2024-04-17 14:33:16 -0300

    refactor

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const CTRGenerator = require('./CRTGenerator');
 const BATGenerator = require('./BATGenerator');
 const TableService = require('./TableService');
 
-const tableService = new TableService(
-	new CTRGenerator(),
-	new BATGenerator('RH_DADOS', 't$rhd4d0s#2023', 'PMSFP_HML'),
-);
+class TableServiceCli {
+	#TableService;
+	constructor(DBuser, DBPassword, DBTns) {
+		this.#TableService = new TableService(
+			new CTRGenerator(),
+			new BATGenerator(DBuser, DBPassword, DBTns),
+		);
+	}
 
-tableService.setRegexForTablename(/.*\.(.*)\..*/);
+	getInstance() {
+		return this.#TableService;
+	}
+}
 
-tableService.setCSVFolder('C:\\Users\\Felipe.admrh01\\Importacao\\Exportação');
-
-tableService.createCTRInto('C:\\Users\\Felipe.admrh01\\Importacao\\CONTROLLER');
-
-tableService.createBATInto('C:\\Users\\Felipe.admrh01\\Importacao\\BAT');
+module.exports = { TableServiceCli };
